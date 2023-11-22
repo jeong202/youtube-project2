7a664f953bb613e5816bb0e6e7083a1bd4095661
jeong202 <jhyjhy968@gmail.com>
2023-11-22 15:13:23 +0900

    20231122/0313
    20231122/0313

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Today from '../components/contents/Today'
 import Youtuber from '../components/contents/Youtuber'
 import Main from '../components/section/Main'
+import VideoSlider from '../components/video/VideoSlider'
+
+import { fetchFromAPI } from '../utils/api'
 
 
 const Home = () => {
+    const [channelVideo, setChannelVideo] = useState([]);
+    const [channelVideo2, setChannelVideo2] = useState([]);
+    const [channelVideo3, setChannelVideo3] = useState([]);
+    const [channelVideo4, setChannelVideo4] = useState([]);
+    const [channelVideo5, setChannelVideo5] = useState([]);
+
+    useEffect(() => {
+        const v1 = "UCNroQMUbFjMUVvhY-Fcjlkw"
+        const v2 = "UCTdMmswUit6aZ9yp-JyDezQ";
+        const v3 = "UCY8X9tLD1xyKMfBpEDCVAKQ";
+        const v4 = "UCDJXyjBhdL3H8527gyxMaEw";
+        const v5 = "UCcjPm1u-7Lmvrfy1thcXvEA";
+
+        const fetchResults = async () => {
+            try {
+                const videosData = await fetchFromAPI(`search?channelId=${v1}&part=snippet&order=date`);
+                setChannelVideo(videosData.items);
+
+                const videosData2 = await fetchFromAPI(`search?channelId=${v2}&part=snippet&order=date`);
+                setChannelVideo2(videosData2.items);
+
+                const videosData3 = await fetchFromAPI(`search?channelId=${v3}&part=snippet&order=date`);
+                setChannelVideo3(videosData3.items);
+
+                const videosData4 = await fetchFromAPI(`search?channelId=${v4}&part=snippet&order=date`);
+                setChannelVideo4(videosData4.items);
+
+                const videosData5 = await fetchFromAPI(`search?channelId=${v5}&part=snippet&order=date`);
+                setChannelVideo5(videosData5.items);
+
+            } catch (error) {
+                console.log('Eroor fetching data', error)
+            }
+        }
+        fetchResults();
+    }, [])
     return (
         <Main
             title="기타 유튜버"
@@ -13,6 +52,12 @@ const Home = () => {
         >
             <Today />
             <Youtuber />
+
+            <VideoSlider videos={channelVideo} title='Emotional Guitarist AZ 최신 영상' name='v1' />
+            <VideoSlider videos={channelVideo2} title='펀투 (funtwo) 최신 영상' name='v2' />
+            <VideoSlider videos={channelVideo3} title='😘 Neil Zaza 최신 영상' name='v3' />
+            <VideoSlider videos={channelVideo4} title='cho pilsung Awesome Show TV 최신 영상' name='v4' />
+            <VideoSlider videos={channelVideo5} title='KYUHEE PARK 최신 영상' name='v5' />
         </Main>
 
 
--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,64 +1,97 @@
-import React from 'react'
-import { todayText } from '../data/today'
+// import React from 'react'
+// import { todayText } from '../data/today'
+// import Main from '../components/section/Main'
+
+// const Today = () => {
+//     return (
+//         <Main
+//             title="오늘의 추천 영상"
+//             description="오늘의 추천 유튜브 영상입니다."
+//         >
+
+//             <section id='todayPage'>
+//                 <h2>오늘의 추천 영상</h2>
+//                 {todayText.map((today, key) => (
+//                     <div className='today__inner' key={key}>
+//                         <div className='today__thumb'>
+//                             <img src={today.img} alt={today.author} />
+//                         </div>
+//                         <div className='today__text'>
+//                             <span className='today'>오늘의 영상!</span>
+//                             <h3 className='title'>
+//                                 {today.title}
+//                             </h3>
+//                             <p className='desc'>{today.desc}</p>
+//                             <div className='info'>
+//                                 <span className='author'>
+//                                     {today.author}
+//                                 </span>
+//                                 <span className='date'>{
+//                                     today.date}
+//                                 </span>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 ))}
+
+//             </section>
+//         </Main>
+
+//     )
+// }
+
+// export default Today
+
+
+import React, { useEffect, useState } from 'react'
 import Main from '../components/section/Main'
 
+import { todayText } from '../data/today'
+import { Link } from 'react-router-dom'
+
 const Today = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 300);
+    }, []);
+
+    const todayPageClass = loading ? 'isLoading' : 'isLoaded';
+
     return (
         <Main
-            title="오늘의 추천 영상"
-            description="오늘의 추천 유튜브 영상입니다."
-        >
+            title="추천 영상"
+            description="오늘의 추천 유튜브 영상입니다.">
 
-            <section id='todayPage'>
+            <section id='todayPage' className={todayPageClass}>
                 <h2>오늘의 추천 영상</h2>
+
                 {todayText.map((today, key) => (
                     <div className='today__inner' key={key}>
-                        <div className='today__thumb'>
-                            <img src={today.img} alt={today.author} />
+                        <div className='today__thumb play__icon'>
+                            <Link to={today.page}>
+                                <img src={today.img} alt={today.title} />
+                            </Link>
                         </div>
                         <div className='today__text'>
-                            <span className='today'>오늘의 영상!</span>
+                            <span className='today'>today!</span>
                             <h3 className='title'>
-                                {today.title}
+                                <Link to={today.page}>{today.title}</Link>
                             </h3>
                             <p className='desc'>{today.desc}</p>
                             <div className='info'>
                                 <span className='author'>
-                                    {today.author}
+                                    <Link to={`/channel/${today.channelId}`}>{today.author}</Link>
                                 </span>
-                                <span className='date'>{
-                                    today.date}
-                                </span>
+                                <span className='date'>{today.date}</span>
                             </div>
                         </div>
                     </div>
                 ))}
-
-                {/* <div className="today__inner">
-                <div className="today__thumb">
-
-                </div>
-                <div className="today__text">
-                    <span className='today'>오늘의 영상!</span>
-                    <h3 className='title'>Nella Fantasia (Gabriel's Oboe) / Singing Guitar by AZ</h3>
-                    <p className='desc'>
-                        Emotional Guitarist AZ는 기타리스트입니다.
-                        일본 기타리스트 팀 G.O.D. 멤버입니다.
-                    </p>
-                    <div className="info">
-                        <div className="author">
-                            Emotional Guitarist AZ
-                        </div>
-                        <div className="date">
-                            2022.12.25
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-
             </section>
         </Main>
-
     )
 }
 
