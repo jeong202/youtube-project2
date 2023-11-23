995492335d83eb79f8bbe33dfce949e808b0ae0b
jeong202 <jhyjhy968@gmail.com>
2023-11-23 10:12:52 +0900

    20231123/10-12
    20231123/10-12

--- src/components/contents/Today.jsx
+++ src/components/contents/Today.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { todayText } from '../../data/today'
-
+import { Link } from 'react-router-dom'
 
 
 const Today = () => {
     return (
         <section id='today'>
             <div className="today__inner">
-                <div className="today__thumb">
+                <div className="today__thumb play__icon">
+                    <Link
+                        to={`/video/${todayText[0].videoId}`}
+                        style={{ backgroundImage: `url(${todayText[0].img})` }}
+                    >
+                    </Link>
+
                 </div>
                 <div className="today__text">
                     <span className='today'>오늘의 영상!</span>
@@ -30,49 +36,3 @@ const Today = () => {
 }
 
 export default Today
-// import React, { useEffect, useState } from 'react'
-
-// import { Link } from 'react-router-dom'
-
-// const Today = ({ videos, id }) => {
-//     const [loading, setLoading] = useState(true);
-
-//     useEffect(() => {
-//         setTimeout(() => {
-//             setLoading(false)
-//         }, 300);
-//     }, []);
-
-//     const todayClass = loading ? 'isLoading' : 'isLoaded'
-
-//     return (
-//         <section id={id} className={todayClass}>
-//             <div className='today__inner'>
-//                 <div className='today__thumb play__icon'>
-//                     <Link to={videos[0].page}>
-//                         <img src={videos[0].img} alt={videos[0].title} />
-//                     </Link>
-//                 </div>
-//                 <div className='today__text'>
-//                     <span className='today'>today!</span>
-//                     <h3 className='title'>
-//                         <Link to={videos[0].page}>
-//                             {videos[0].title}
-//                         </Link>
-//                     </h3>
-//                     <p className='desc'>{videos[0].desc}</p>
-//                     <div className='info'>
-//                         <span className='author'>
-//                             <Link to={`/channel/${videos[0].channelId}`}>
-//                                 {videos[0].author}
-//                             </Link>
-//                         </span>
-//                         <span className='date'>{videos[0].date}</span>
-//                     </div>
-//                 </div>
-//             </div>
-//         </section>
-//     )
-// }
-
-// export default Today
