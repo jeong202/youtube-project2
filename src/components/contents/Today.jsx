a57c14e4427fa3af7007f3f55a10cb1530aa94c4
jeong202 <jhyjhy968@gmail.com>
2023-11-23 09:56:13 +0900

    20231123/09-56
    20231123/09-56

--- src/components/contents/Today.jsx
+++ src/components/contents/Today.jsx
@@ -1,73 +1,27 @@
-// import React from 'react'
-// import { todayText } from '../../data/today'
+import React from 'react'
+import { todayText } from '../../data/today'
 
 
 
-// const Today = () => {
-//     return (
-//         <section id='today'>
-//             <div className="today__inner">
-//                 <div className="today__thumb">
-//                 </div>
-//                 <div className="today__text">
-//                     <span className='today'>오늘의 영상!</span>
-//                     <h3 className='title'>{todayText[0].title}</h3>
-//                     <p className='desc'>
-//                         {todayText[0].desc}
-//                     </p>
-//                     <div className="info">
-//                         <div className="author">
-//                             {todayText[0].author}
-//                         </div>
-//                         <div className="date">
-//                             {todayText[0].date}
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </section>
-//     )
-// }
-
-// export default Today
-import React, { useEffect, useState } from 'react'
-
-import { Link } from 'react-router-dom'
-
-const Today = ({ videos, id }) => {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false)
-        }, 300);
-    }, []);
-
-    const todayClass = loading ? 'isLoading' : 'isLoaded'
-
+const Today = () => {
     return (
-        <section id={id} className={todayClass}>
-            <div className='today__inner'>
-                <div className='today__thumb play__icon'>
-                    <Link to={videos[0].page}>
-                        <img src={videos[0].img} alt={videos[0].title} />
-                    </Link>
+        <section id='today'>
+            <div className="today__inner">
+                <div className="today__thumb">
                 </div>
-                <div className='today__text'>
-                    <span className='today'>today!</span>
-                    <h3 className='title'>
-                        <Link to={videos[0].page}>
-                            {videos[0].title}
-                        </Link>
-                    </h3>
-                    <p className='desc'>{videos[0].desc}</p>
-                    <div className='info'>
-                        <span className='author'>
-                            <Link to={`/channel/${videos[0].channelId}`}>
-                                {videos[0].author}
-                            </Link>
-                        </span>
-                        <span className='date'>{videos[0].date}</span>
+                <div className="today__text">
+                    <span className='today'>오늘의 영상!</span>
+                    <h3 className='title'>{todayText[0].title}</h3>
+                    <p className='desc'>
+                        {todayText[0].desc}
+                    </p>
+                    <div className="info">
+                        <div className="author">
+                            {todayText[0].author}
+                        </div>
+                        <div className="date">
+                            {todayText[0].date}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -76,3 +30,49 @@ const Today = ({ videos, id }) => {
 }
 
 export default Today
+// import React, { useEffect, useState } from 'react'
+
+// import { Link } from 'react-router-dom'
+
+// const Today = ({ videos, id }) => {
+//     const [loading, setLoading] = useState(true);
+
+//     useEffect(() => {
+//         setTimeout(() => {
+//             setLoading(false)
+//         }, 300);
+//     }, []);
+
+//     const todayClass = loading ? 'isLoading' : 'isLoaded'
+
+//     return (
+//         <section id={id} className={todayClass}>
+//             <div className='today__inner'>
+//                 <div className='today__thumb play__icon'>
+//                     <Link to={videos[0].page}>
+//                         <img src={videos[0].img} alt={videos[0].title} />
+//                     </Link>
+//                 </div>
+//                 <div className='today__text'>
+//                     <span className='today'>today!</span>
+//                     <h3 className='title'>
+//                         <Link to={videos[0].page}>
+//                             {videos[0].title}
+//                         </Link>
+//                     </h3>
+//                     <p className='desc'>{videos[0].desc}</p>
+//                     <div className='info'>
+//                         <span className='author'>
+//                             <Link to={`/channel/${videos[0].channelId}`}>
+//                                 {videos[0].author}
+//                             </Link>
+//                         </span>
+//                         <span className='date'>{videos[0].date}</span>
+//                     </div>
+//                 </div>
+//             </div>
+//         </section>
+//     )
+// }
+
+// export default Today
--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,98 +1,45 @@
-// import React from 'react'
-// import { todayText } from '../data/today'
-// import Main from '../components/section/Main'
-
-// const Today = () => {
-//     return (
-//         <Main
-//             title="오늘의 추천 영상"
-//             description="오늘의 추천 유튜브 영상입니다."
-//         >
-
-//             <section id='todayPage'>
-//                 <h2>오늘의 추천 영상</h2>
-//                 {todayText.map((today, key) => (
-//                     <div className='today__inner' key={key}>
-//                         <div className='today__thumb'>
-//                             <img src={today.img} alt={today.author} />
-//                         </div>
-//                         <div className='today__text'>
-//                             <span className='today'>오늘의 영상!</span>
-//                             <h3 className='title'>
-//                                 {today.title}
-//                             </h3>
-//                             <p className='desc'>{today.desc}</p>
-//                             <div className='info'>
-//                                 <span className='author'>
-//                                     {today.author}
-//                                 </span>
-//                                 <span className='date'>{
-//                                     today.date}
-//                                 </span>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 ))}
-
-//             </section>
-//         </Main>
-
-//     )
-// }
-
-// export default Today
-
-
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { todayText } from '../data/today'
 import Main from '../components/section/Main'
 
-import { todayText } from '../data/today'
-import { Link } from 'react-router-dom'
-
 const Today = () => {
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 300);
-    }, []);
-
-    const todayPageClass = loading ? 'isLoading' : 'isLoaded';
-
     return (
         <Main
-            title="추천 영상"
-            description="오늘의 추천 유튜브 영상입니다.">
+            title="오늘의 추천 영상"
+            description="오늘의 추천 유튜브 영상입니다."
+        >
 
-            <section id='todayPage' className={todayPageClass}>
+            <section id='todayPage'>
                 <h2>오늘의 추천 영상</h2>
-
                 {todayText.map((today, key) => (
                     <div className='today__inner' key={key}>
-                        <div className='today__thumb play__icon'>
-                            <Link to={today.page}>
-                                <img src={today.img} alt={today.title} />
-                            </Link>
+                        <div className='today__thumb'>
+                            <img src={today.img} alt={today.author} />
                         </div>
                         <div className='today__text'>
-                            <span className='today'>today!</span>
+                            <span className='today'>오늘의 영상!</span>
                             <h3 className='title'>
-                                <Link to={today.page}>{today.title}</Link>
+                                {today.title}
                             </h3>
                             <p className='desc'>{today.desc}</p>
                             <div className='info'>
                                 <span className='author'>
-                                    <Link to={`/channel/${today.channelId}`}>{today.author}</Link>
+                                    {today.author}
                                 </span>
-                                <span className='date'>{today.date}</span>
+                                <span className='date'>{
+                                    today.date}
+                                </span>
                             </div>
                         </div>
                     </div>
                 ))}
+
             </section>
         </Main>
+
     )
 }
 
 export default Today
+
+
